3e2c84d73d010719cd79dfb0b61394afa165ec00
qiyuangong <qiyuangong@gmail.com>
2015-05-12 21:01:39 +0800

    Delete useless codes.

--- top_down_greedy_anonymization.py
+++ top_down_greedy_anonymization.py
@@ -13,6 +13,7 @@ gl_K = 0
 gl_result = []
 gl_att_trees = []
 gl_QI_range = []
+gl_rounds = 3
 
 
 class Partition:
@@ -46,7 +47,7 @@ def list_to_str(value_list, cmpfun=cmp, sep=';'):
     return sep.join(temp)
 
 
-def check_L_diversity(partition):
+def check_K_anonymity(partition):
     """check if partition satisfy l-diversity
     return True if satisfy, False if not.
     """
@@ -55,15 +56,25 @@ def check_L_diversity(partition):
         ltemp = partition.member
     else:
         ltemp = partition
-    for temp in ltemp:
-        stemp = list_to_str(temp[-1])
-        try:
-            sa_dict[stemp] += 1
-        except:
-            sa_dict[stemp] = 1
-            if len(sa_dict) >= gl_K:
-                return True
+    if len(ltemp) >= gl_K:
+        return True
     return False
+
+
+def NCP(record1, record2):
+    pass
+
+
+def middle(record1, record2):
+    pass
+
+
+def balance():
+    pass
+
+
+def get_pair():
+    pass
 
 
 def cmp_str(element1, element2):
@@ -80,157 +91,13 @@ def getNormalizedWidth(partition, index):
     return width * 1.0 / gl_QI_range[index]
 
 
-def choose_dimension(partition):
-    """chooss dim with largest normlized Width
-    return dim index.
-    """
-    max_witdh = -1
-    max_dim = -1
-    for i in range(gl_QI_len):
-        if partition.allow[i] == 0:
-            continue
-        normWidth = getNormalizedWidth(partition, i)
-        if normWidth > max_witdh:
-            max_witdh = normWidth
-            max_dim = i
-    if max_witdh > 1:
-        print "Error: max_witdh > 1"
-        pdb.set_trace()
-    return max_dim
-
-
-def frequency_set(partition, dim):
-    """get the frequency_set of partition on dim
-    return dict{key: str values, values: count}
-    """
-    frequency = {}
-    for record in partition.member:
-        try:
-            frequency[record[dim]] += 1
-        except:
-            frequency[record[dim]] = 1
-    return frequency
-
-
-def find_median(frequency):
-    """find the middle of the partition
-    return splitVal
-    """
-    splitVal = ''
-    value_list = frequency.keys()
-    value_list.sort(cmp=cmp_str)
-    total = sum(frequency.values())
-    middle = total / 2
-    if middle < gl_K:
-        print "Error: size of group less than 2*K"
-        return ''
-    index = 0
-    split_index = 0
-    for i, t in enumerate(value_list):
-        index += frequency[t]
-        if index >= middle:
-            splitVal = t
-            split_index = i
-            break
-    else:
-        print "Error: cannot find splitVal"
-    return (splitVal, split_index)
-
-
 def anonymize(partition):
     """
-    Main procedure of mondrian_l_diversity.
+    Main procedure of top_down_greedy_anonymization.
     recursively partition groups until not allowable.
     """
-    global gl_result
-    if len(partition.member) < 2 * gl_K:
-        gl_result.append(partition)
-        return
-    allow_count = sum(partition.allow)
-    pwidth = partition.width
-    pmiddle = partition.middle
-    # pallow = partition.allow
-    for index in range(allow_count):
-        dim = choose_dimension(partition)
-        if dim == -1:
-            print "Error: dim=-1"
-            pdb.set_trace()
-        if isinstance(gl_att_trees[dim], NumRange):
-            # numeric attributes
-            frequency = frequency_set(partition, dim)
-            (splitVal, split_index) = find_median(frequency)
-            if splitVal == '':
-                print "Error: splitVal= null"
-                pdb.set_trace()
-            middle_pos = gl_att_trees[dim].dict[splitVal]
-            lmiddle = pmiddle[:]
-            rmiddle = pmiddle[:]
-            temp = pmiddle[dim].split(',')
-            low = temp[0]
-            high = temp[1]
-            lmiddle[dim] = low + ',' + splitVal
-            rmiddle[dim] = splitVal + ',' + high
-            lhs = []
-            rhs = []
-            for temp in partition.member:
-                pos = gl_att_trees[dim].dict[temp[dim]]
-                if pos <= middle_pos:
-                    # lhs = [low, means]
-                    lhs.append(temp)
-                else:
-                    # rhs = (means, high]
-                    rhs.append(temp)
-            lwidth = pwidth[:]
-            rwidth = pwidth[:]
-            lwidth[dim] = split_index + 1
-            rwidth[dim] = pwidth[dim] - split_index - 1
-            if check_L_diversity(lhs) is False or check_L_diversity(rhs) is False:
-                partition.allow[dim] = 0
-                continue
-            # anonymize sub-partition
-            anonymize(Partition(lhs, lwidth, lmiddle))
-            anonymize(Partition(rhs, rwidth, rmiddle))
-            return
-        else:
-            # normal attributes
-            if partition.middle[dim] != '*':
-                splitVal = gl_att_trees[dim][partition.middle[dim]]
-            else:
-                splitVal = gl_att_trees[dim]['*']
-            sub_node = [t for t in splitVal.child]
-            sub_partition = []
-            for i in range(len(sub_node)):
-                sub_partition.append([])
-            for temp in partition.member:
-                qid_value = temp[dim]
-                for i, node in enumerate(sub_node):
-                    try:
-                        node.cover[qid_value]
-                        sub_partition[i].append(temp)
-                        break
-                    except:
-                        continue
-            flag = True
-            for p in sub_partition:
-                if len(p) == 0:
-                    continue
-                if check_L_diversity(p) is False:
-                    flag = False
-                    break
-            if flag:
-                for i, p in enumerate(sub_partition):
-                    if len(p) == 0:
-                        continue
-                    wtemp = pwidth[:]
-                    mtemp = pmiddle[:]
-                    wtemp[dim] = sub_node[i].support
-                    mtemp[dim] = sub_node[i].value
-                    anonymize(Partition(p, wtemp, mtemp))
-                return
-            else:
-                partition.allow[dim] = 0
-                continue
-    gl_result.append(partition)
+    
+    
 
 
 def Top_Down_Greedy_Anonymization(att_trees, data, K):
